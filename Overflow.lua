96a00ac6ed5227e08e0b7f5329396bc78e35bb71
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-05-28 21:29:25 +0100

    fix nostalgic deck perkeo

--- Overflow.lua
+++ Overflow.lua
@@ -29,27 +29,40 @@ SMODS.Joker:take_ownership("j_perkeo", {
                     cards[#cards+1] = {(v.ability.immutable.overflow_amount or 1), v}
                 end
                 local card = Overflow.weighted_random(cards, "perkeo")
-                if card.ability.immutable.overflow_amount and Overflow.can_merge(card, card, true) then
-                    Overflow.set_amount(card, card.ability.immutable.overflow_amount + 1)
-                else
-                    local check
-                    for i, v in ipairs(G.consumeables.cards) do
-                        if v.edition and v.edition.key == "e_negative" and v.config.center.key == card.config.center.key and v ~= card then
-                            Overflow.set_amount(v, (v.ability.immutable.overflow_amount or 1) + 1)
-                            check = true
+                if card.config.center.set == "Joker" then
+                    G.E_MANAGER:add_event(Event({
+                        func = function() 
+                            local new_card = copy_card(card, nil)
+                            new_card.ability.immutable.overflow_amount = 1
+                            new_card:set_edition("e_negative", true)
+                            new_card:add_to_deck()
+                            G.consumeables:emplace(new_card) 
+                            return true
                         end
-                    end
-                    if not check then
-                        G.E_MANAGER:add_event(Event({
-                            func = function() 
-                                local new_card = copy_card(card, nil)
-                                new_card.ability.immutable.overflow_amount = 1
-                                new_card:set_edition("e_negative", true)
-                                new_card:add_to_deck()
-                                G.consumeables:emplace(new_card) 
-                                return true
+                    }))
+                else
+                    if card.ability.immutable.overflow_amount and Overflow.can_merge(card, card, true) then
+                        Overflow.set_amount(card, card.ability.immutable.overflow_amount + 1)
+                    else
+                        local check
+                        for i, v in ipairs(G.consumeables.cards) do
+                            if v.edition and v.edition.key == "e_negative" and v.config.center.key == card.config.center.key and v ~= card then
+                                Overflow.set_amount(v, (v.ability.immutable.overflow_amount or 1) + 1)
+                                check = true
                             end
-                        }))
+                        end
+                        if not check then
+                            G.E_MANAGER:add_event(Event({
+                                func = function() 
+                                    local new_card = copy_card(card, nil)
+                                    new_card.ability.immutable.overflow_amount = 1
+                                    new_card:set_edition("e_negative", true)
+                                    new_card:add_to_deck()
+                                    G.consumeables:emplace(new_card) 
+                                    return true
+                                end
+                            }))
+                        end
                     end
                 end
                 card_eval_status_text(context.blueprint_card or orig_card, 'extra', nil, nil, nil, {message = localize('k_duplicated_ex')})
